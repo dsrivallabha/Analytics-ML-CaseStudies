550b7fcd58c1fd6520036af92f19ab5bfc945af2
dsrivallabha <srivallabhad@yahoo.com>
2022-12-31 10:34:02 +0530

    removing unwanted repos

--- DashBoards/clifford-master/panelserverextension.py
+++
@@ -1,5 +0,0 @@
-from subprocess import Popen
-
-def load_jupyter_server_extension(nbapp):
-    """serve the clifford.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "clifford.ipynb", "--allow-websocket-origin=*"])
